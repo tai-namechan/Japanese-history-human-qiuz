b6b94cfe2deba7d1524cb026dc14db2b87bd8ef2
KakamuItsuki <84170149+KakamuItsuki@users.noreply.github.com>
2022-01-19 22:53:21 +0900

    スタートにバナー広告

--- Advertisement/Ads.js
+++ Advertisement/Ads.js
@@ -15,7 +15,7 @@ export const BottomBannerAds = () => (
     adUnitID={
         __DEV__ ? "ca-app-pub-3940256099942544/6300978111" // テスト広告
         : Platform.select({
-          ios: "ca-app-pub-8494354802155058/8903720559" , // iOS
+          ios: "ca-app-pub-3488366423563341/5687545601" , // iOS
           android:"広告ユニットID" , // android 
         })
       } // Test ID, Replace with your-admob-unit-id
